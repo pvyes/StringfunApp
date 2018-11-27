a3015011ef42955ff026da90c5b9e779687b1030
Samuel Sette <settesam@live.be>
2018-11-27 16:56:38 +0100

    Added models folder with classes

---
+++ StringFunApp.Solution/StringFunApp/StringFunApp/ClassLibrary/Models/Instrument.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringFunApp.ClassLibrary.Models
+{
+    public abstract class Instrument
+    {
+        public static Instrument IsViool()
+        {
+            return new Viool() { Naam = "Viool" };
+        }
+
+        public static Instrument IsAltviool()
+        {
+            return new Altviool() { Naam = "Altviool" };
+        }
+
+        public static Instrument IsCello()
+        {
+            return new Cello() { Naam = "Cello" };
+        }
+    }
+}
---
+++ StringFunApp.Solution/StringFunApp/StringFunApp/ClassLibrary/Models/Stap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringFunApp.ClassLibrary.Models
+{
+    public class Stap
+    {
+        public Guid Id { get; set; }
+        public Instrument Instrument { get; set; }
+        public int BoekNummer { get; set; }
+        public List<VideoInfo> Videos { get; set; }
+    }
+}
