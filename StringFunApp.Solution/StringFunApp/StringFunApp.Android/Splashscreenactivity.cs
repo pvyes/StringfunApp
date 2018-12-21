17ed4e5264e22f72994f5ac90ce04505d6ad7347
Samuel Sette <settesam@live.be>
2018-12-21 11:08:59 +0100

    Fixed android splashscreen

--- StringFunApp.Solution/StringFunApp/StringFunApp.Android/Splashscreenactivity.cs
+++ StringFunApp.Solution/StringFunApp/StringFunApp.Android/Splashscreenactivity.cs
@@ -5,6 +5,7 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Content.PM;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -12,7 +13,9 @@ using Android.Widget;
 
 namespace StringFunApp.Droid
 {
-    [Activity(Label = "Splashscreenactivity")]
+    [Activity(Label = "Splashscreenactivity", Icon = "@drawable/iconm",
+              Theme = "@style/MainTheme.Splash", MainLauncher = true,
+              ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class Splashscreenactivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
